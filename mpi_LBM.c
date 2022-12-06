049ecf824b9333d209959f7b31db53866c2820c5
Khai-Ngo <ngoduykhai2001@gmail.com>
2022-12-06 17:33:34 +0000

    No segfault until block row exchanges

--- mpi_LBM.c
+++ mpi_LBM.c
@@ -8,7 +8,7 @@
 // python shall do the magic of transposing the matrix and plot it as flow from left to right again 
 // also obvious that it's based on heat equation code 
 
-void initLattice(int lat_size, float *f){
+/*void initLattice(int lat_size, float *f){
     // initialize the lattice in master task. Note f[1] is not touched since there's no need for it. 
     // master shall later received results from each worker and paste it into this initialized lattice
     for (int i = 0; i <lat_size ;i++){
@@ -17,7 +17,7 @@ void initLattice(int lat_size, float *f){
                 *(f+i*9+k) = 0;
             } 
     }       
-}
+}*/
 void updateRUV(int lat_size, int* ex, int* ey, float *rho, float *xVel, float *yVel, float *f){
     // probably to be run in the master task only
     float vx, vy, density;
@@ -38,17 +38,17 @@ void updateRUV(int lat_size, int* ex, int* ey, float *rho, float *xVel, float *y
         *(yVel+i) = vy;
     }
 }
-void collision(int* ex, int* ey, float* weights,float rho, float xVel, float yVel, float *f, float tau){
-    // not suppose to update rho, xVel, and yVel so pass them by value
-    // update *f so pass by reference
-    double dotProd, uProd, fiEQ_i;
-    for (int i = 0; i<9;i++){
-        dotProd = ex[i]*xVel+ey[i]*yVel;
-        uProd = xVel*xVel+yVel*yVel;
-        fiEQ_i= weights[i]*rho*(1+3*dotProd+4.5*dotProd*dotProd-1.5*uProd);
-        *(f+i) -= (*(f+i)-fiEQ_i)/tau;
-    }
-}
+// void collision(int* ex, int* ey, float* weights,float rho, float xVel, float yVel, float *f, float tau){
+//     // not suppose to update rho, xVel, and yVel so pass them by value
+//     // update *f so pass by reference
+//     double dotProd, uProd, fiEQ_i;
+//     for (int i = 0; i<9;i++){
+//         dotProd = ex[i]*xVel+ey[i]*yVel;
+//         uProd = xVel*xVel+yVel*yVel;
+//         fiEQ_i= weights[i]*rho*(1+3*dotProd+4.5*dotProd*dotProd-1.5*uProd);
+//         *(f+i) -= (*(f+i)-fiEQ_i)/tau;
+//     }
+// }
 void exportMap(char* fname, int l, int w, float* map){
     // visualizer programm shall need to transpose this
     int lat_size = l*w;
@@ -67,7 +67,7 @@ int main(int argc, char* argv[]){
     void initLattice(), ruv(), exportMap();
     // MPI related variables
     int taskid, numworkers, numtasks, 
-    averow, rows, offset, extra,
+    aveBlock, chunks, offset, extra, averow,
     dest, src, 
     left, right,
     msgtype, 
@@ -122,10 +122,18 @@ int main(int argc, char* argv[]){
     }
     // only master prints stuff and initialize lattice
     printf("LBM of a %dx%d channel flow for %d_sec, west side inlet u0 = 0.2. %d worker tasks\n", l, w, time, numworkers);
-    initLattice(lat_size, &f[0][0][0]); // note f stores the address of f[0][0][0] (i.e. f_0 at (0, 0) of the current lattice. f[1][0][0] is f_0 at (0,0) for the duplicate lattice)
+    //initLattice(lat_size, &f[0][0][0]); // note f stores the address of f[0][0][0] (i.e. f_0 at (0, 0) of the current lattice. f[1][0][0] is f_0 at (0,0) for the duplicate lattice)
+    for (int k =0; k < 2; k++){
+        for (int i=0;i<lat_size;i++){
+            f[k][i][0]=1;
+            for (int j=1;j<9;j++){
+                f[k][i][j]=0;
+            }
+        }
+    }
     updateRUV(lat_size, ex, ey, &rho[0], &xVel[0], &yVel[0], &f[0][0][0]);
-    //exportMap("init_rho.txt", l, w, &rho[0]); //sanity check
-    //exportMap("init_xVel.txt", l, w, &xVel[0]);  //sanity check
+    //exportMap("init_rho_new.txt", l, w, &rho[0]); //sanity check
+    //exportMap("init_xVel_new.txt", l, w, &xVel[0]);  //sanity check
     
     // start timing from when you start sending work (all the f_i of every lattice point, and the array partitions)
     // cheat a bit by not including lattice initialization
@@ -138,7 +146,7 @@ int main(int argc, char* argv[]){
     for (unsigned i =1 ; i<numworkers+1;i++){
         dest = i;
         // each task take on 1 extra row, until you've got no extra row (worst case everyone took on an extra row except for 1 task)
-        rows=(i<=extra)? averow+1:averow; 
+        chunks= (i<=extra)? (averow+1)*w:averow*w; 
         // Tell workers who their neighbours are
         if (i==1)
             left = numworkers; // left flow loops all the way to the back 
@@ -149,27 +157,27 @@ int main(int argc, char* argv[]){
         else right = i+1;
         // Send startup info to each worker
         MPI_Send(&offset, 1, MPI_INT, dest, BEGIN, MPI_COMM_WORLD);
-        MPI_Send(&rows, 1, MPI_INT, dest, BEGIN, MPI_COMM_WORLD);
+        MPI_Send(&chunks, 1, MPI_INT, dest, BEGIN, MPI_COMM_WORLD);
         MPI_Send(&left, 1, MPI_INT, dest, BEGIN, MPI_COMM_WORLD);
         MPI_Send(&right, 1, MPI_INT, dest, BEGIN, MPI_COMM_WORLD);
-        MPI_Send(&f[0][offset][0], rows*w*9, MPI_FLOAT, dest, BEGIN,MPI_COMM_WORLD);
-        MPI_Send(&rho[offset], rows*w, MPI_FLOAT, dest, BEGIN, MPI_COMM_WORLD);
-        MPI_Send(&xVel[offset], rows*w, MPI_FLOAT, dest, BEGIN, MPI_COMM_WORLD);
-        MPI_Send(&yVel[offset], rows*w, MPI_FLOAT, dest, BEGIN, MPI_COMM_WORLD);
-        printf("Sent to task %d: rows= %d offset= %d ",dest,rows,offset);
+        MPI_Send(&f[0][offset][0], chunks*9, MPI_FLOAT, dest, BEGIN,MPI_COMM_WORLD);
+        MPI_Send(&rho[offset], chunks, MPI_FLOAT, dest, BEGIN, MPI_COMM_WORLD);
+        MPI_Send(&xVel[offset], chunks, MPI_FLOAT, dest, BEGIN, MPI_COMM_WORLD);
+        MPI_Send(&yVel[offset], chunks, MPI_FLOAT, dest, BEGIN, MPI_COMM_WORLD);
+        printf("Sent to task %d: chunks= %d offset= %d ",dest,chunks,offset);
         printf("left= %d right= %d\n",left,right);
-        offset += rows*w;
+        offset += chunks;
     }
     // now wait for work done by loyal workers
     for (unsigned i = 1; i< numworkers+1;i++){
         src = i;
         msgtype = DONE;
         MPI_Recv(&offset, 1, MPI_INT, src, msgtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&rows, 1, MPI_INT, src, msgtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&f[0][offset][0], rows*w*9, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&rho[offset], rows*w, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&xVel[offset], rows*w, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&yVel[offset], rows*w, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&chunks, 1, MPI_INT, src, msgtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&f[0][offset][0], chunks*9, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&rho[offset], chunks, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&xVel[offset], chunks, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&yVel[offset], chunks, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
     }
     // stop timing when workers have received, done its share of work, and sent back all f_i data to master
     finalTime = MPI_Wtime() - startTime;
@@ -191,63 +199,76 @@ int main(int argc, char* argv[]){
     }
     // WORKERS DO THE BELOW
     if (taskid!=MASTER){
-        printf("heree!!!!!!!!!!!!!!!!11");
         // worker creates the same grid lattice, but they only work on their given portion
-       // i guess just initialize everything to zero first?
-       for (int k = 0; k<2;k++)
-        for (int i = 0; i<lat_size;i++)
-            for (int j=0;j<9;j++)
+       // Initialize lattice just like master first
+       for (int k =0; k < 2; k++){
+        for (int i=0;i<lat_size;i++){
+            f[k][i][0]=1;
+            for (int j=1;j<9;j++){
                 f[k][i][j]=0;
-            
+                }
+            }
+        }
         // receive its share of the lattice
         src = MASTER;
         msgtype = BEGIN;
         MPI_Recv(&offset, 1, MPI_INT, src, msgtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&rows, 1, MPI_INT, src, msgtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&chunks, 1, MPI_INT, src, msgtype, MPI_COMM_WORLD, &status);
         MPI_Recv(&left, 1, MPI_INT, src, msgtype, MPI_COMM_WORLD, &status);
         MPI_Recv(&right, 1, MPI_INT, src, msgtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&f[0][offset][0], rows*w*9, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&rho[offset], rows*w, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&xVel[offset], rows*w, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&yVel[offset], rows*w, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&f[0][offset][0], chunks*9, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&rho[offset], chunks, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&xVel[offset], chunks, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
+        MPI_Recv(&yVel[offset], chunks, MPI_FLOAT, src, msgtype, MPI_COMM_WORLD, &status);
         // process its share of the lattice 
         start = offset;
-        end = offset+rows*w;
-        int e,w,n,s,ne,sw,nw,se;
+        end = offset+chunks;
+        int rowno, colno, 
+        e,west,n,s,ne,sw,nw,se;
+        float dotProd, uProd, fiEQ_i;
         int z = 0; 
         for (int t=0;t<time;t++){
             for (int i=start; i<end;i++){
             // collision
-            collision(ex, ey, weights,rho[i], xVel[i], yVel[i], &f[z][i][0], tau);
+            for (int j = 0;j<9;j++){
+                dotProd = ex[j]*xVel[i]+ey[j]*yVel[i];
+                uProd = xVel[i]*xVel[i] + yVel[i]*yVel[i];
+                fiEQ_i = weights[j]*rho[i]*(1+3*dotProd+4.5*dotProd*dotProd-1.5*uProd);
+                f[z][i][j] -= (f[z][i][j]-fiEQ_i)/tau;
+            }
             // propagation. Some of these checks could be reduced per task if you write more code. 
             // i.e. north south looping only really occurs in task 1 and last task.
-            e = (!(i%w == (w-1)))? i+1 : i+1-w;
-            w = (!(i%w == 0))? i-1:i-1+w;
-            n = (!(i/w == 0))? i-w : i - w + lat_size; 
-            s = (!(i/w == (w-1)))? i+w : i + w - lat_size;
-            ne = (!(i%w == (w-1)))? i-w+1 : i-2*w+1;
+            rowno = i/w;
+            colno = i%w;
+            e = ((colno != (w-1)))? i+1 : i+1-w;
+            west = ((colno != 0))? i-1:i-1+w;
+            n =  (rowno != 0) ? i-w : i - w + lat_size; 
+            s = (rowno != (l-1))? i+w : i + w - lat_size;
+            ne = ((colno != (w-1)))? i-w+1 : i-2*w+1;
             if (ne<0) ne+= lat_size;
-            sw = (!(i%w == 0))? i+w-1:i+2*w-1;
+            sw = ((colno != 0))? i+w-1:i+2*w-1;
             if (sw >=lat_size) sw-=lat_size;
-            nw = (!(i%w == 0))? i-w-1:i-1;
+            nw = ((colno != 0))? i-w-1:i-1;
             if(nw<0) nw+= lat_size;
-            se = (!(i%w == (w-1)))? i+w+1 : i+1;
+            se = ((colno != (w-1)))? i+w+1 : i+1;
             if (se >= lat_size) se-=lat_size;
+            // printf("e=%d, w=%d, n=%d, s=%d, ne=%d, sw=%d, nw=%d, se=%d", e,west,n,s,ne,sw,nw,se);
             f[1-z][i][0] = f[z][i][0]; // e0
             f[1-z][e][1] = f[z][i][1]; // e1 E
-            f[1-z][w][2]=f[z][i][2]; // e2 W 
-            f[1-z][n][3]=f[z][i][3]; // e3 N
-            f[1-z][s][4]=f[z][i][4]; // e4 S
-            f[1-z][ne][5]=f[z][i][5]; // e5 NE
-            f[1-z][sw][6]=f[z][i][6]; // e6 SW
-            f[1-z][nw][7]=f[z][i][7]; // e7 NW
-            f[1-z][se][8]=f[z][i][8]; // e8 SE
+            f[1-z][west][2] = f[z][i][2]; // e2 W 
+            f[1-z][n][3] = f[z][i][3]; // e3 N
+            f[1-z][s][4] = f[z][i][4]; // e4 S
+            f[1-z][ne][5] = f[z][i][5]; // e5 NE
+            f[1-z][sw][6] = f[z][i][6]; // e6 SW
+            f[1-z][nw][7] = f[z][i][7]; // e7 NW
+            f[1-z][se][8] = f[z][i][8]; // e8 SE
+            // printf("Task %d is running this loop at point %d\n",taskid, i);
             }
+            printf("in before row sending from tsk %d\n", taskid);
             // communicate with neighbors
-            printf("here.");
             // send row before 1st row of this block (that this block propagated into) to above, or last row to the last block if left == numworkers
             if (left == numworkers){
-                MPI_Send(&f[z][w*(l-1)][0], w*9, MPI_FLOAT, left, RTAG, MPI_COMM_WORLD);
+                MPI_Send(&f[z][lat_size-w][0], w*9, MPI_FLOAT, left, RTAG, MPI_COMM_WORLD);
             }
             else{
                 MPI_Send(&f[z][start - w][0], w*9, MPI_FLOAT, left, RTAG, MPI_COMM_WORLD);
@@ -281,6 +302,7 @@ int main(int argc, char* argv[]){
                 f[1-z][i][5] = f[z][i][5];
                 f[1-z][i][7] = f[z][i][7];
             }
+            printf("In before BC from task %d", taskid);
             // apply boundary condition
             // left and right sidewalls
             for (int i = start; i<end-w;i+=w){
@@ -314,16 +336,16 @@ int main(int argc, char* argv[]){
                     f[1-z][i][8] = f8update;
                 }
             }
-            updateRUV(rows*w, ex, ey, &rho[start], &xVel[start], &yVel[start], &f[1-z][start][0]);
+            updateRUV(chunks, ex, ey, &rho[start], &xVel[start], &yVel[start], &f[1-z][start][0]);
             z = 1-z; // the other set is the dummy update set
         }
         // finally send work back to master
         MPI_Send(&offset, 1, MPI_INT, MASTER, DONE, MPI_COMM_WORLD);
-        MPI_Send(&rows, 1, MPI_INT, MASTER, DONE, MPI_COMM_WORLD);
-        MPI_Send(&f[0][offset][0], rows*w*9, MPI_FLOAT, MASTER, DONE,MPI_COMM_WORLD);
-        MPI_Send(&rho[offset], rows*w, MPI_FLOAT, MASTER, DONE, MPI_COMM_WORLD);
-        MPI_Send(&xVel[offset], rows*w, MPI_FLOAT, MASTER, DONE, MPI_COMM_WORLD);
-        MPI_Send(&yVel[offset], rows*w, MPI_FLOAT, MASTER, DONE, MPI_COMM_WORLD);
+        MPI_Send(&chunks, 1, MPI_INT, MASTER, DONE, MPI_COMM_WORLD);
+        MPI_Send(&f[1-z][offset][0], chunks*9, MPI_FLOAT, MASTER, DONE,MPI_COMM_WORLD);
+        MPI_Send(&rho[offset], chunks, MPI_FLOAT, MASTER, DONE, MPI_COMM_WORLD);
+        MPI_Send(&xVel[offset], chunks, MPI_FLOAT, MASTER, DONE, MPI_COMM_WORLD);
+        MPI_Send(&yVel[offset], chunks, MPI_FLOAT, MASTER, DONE, MPI_COMM_WORLD);
         MPI_Finalize();
     }
     return 1;
